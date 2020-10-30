08a26bec577056cfda16cf8f9669b692c0ab0755
fenghua-mly <1184119358@qq.com>
2020-10-30 21:02:28 +0800

    第三遍

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,51 @@
 // 入口函数
 $(function() {
-            // 获取用户信息，头像，用户名
-            getUserInfo()
+        // 获取用户信息，头像，用户名
+        getUserInfo()
 
-            var layui = layui.layer
-            $('#btnLogout').on('click', function() {
-                layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function(index) {
-                    //do something
-                    // 清除本地
-                    localStorage.removeItem('token')
-                        // 跳转登录
-                    location.href = '/login.html'
-                        // 关闭询问框
-                    layer.close(index);
-                })
-            });
-            // 封装在入口函数之外
-            // 其他页面也要调用
-            function getUserInfo() {
-                $.ajax({
-                    url: '/my/userinfo',
-                    // 以 /my 开头的请求路径，需要在请求头中携带 
-                    // Authorization 身份认证字段，才能正常访问成功
+        var layer = layui.layer
+        $('#btnLogout').on('click', function() {
+            layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function(index) {
+                //do something
+                // 清除本地
+                localStorage.removeItem('token')
+                    // 跳转登录
+                location.href = '/login.html'
+                    // 关闭询问框
+                layer.close(index);
+            })
+        });
+    })
+    // 封装在入口函数之外
+    // 其他页面也要调用
+function getUserInfo() {
 
-                    success: function(res) {
-                        if (res.status !== 0) {
-                            return layui.layer.msg(res.message);
-                        }
-                        renderAvatar(res.data);
-                    }
-                })
+    $.ajax({
+        url: '/my/userinfo',
+        // 以 /my 开头的请求路径，需要在请求头中携带 
+        // Authorization 身份认证字段，才能正常访问成功
+
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message);
             }
-            // 封装函数头像内容
-            function renderAvatar(user) {
-                var name = user.nickname || user.username;
-                $('.welcome').html("欢迎&nbsp;&nbsp" + name)
+            renderAvatar(res.data);
+        }
+    })
+}
+// 封装函数头像内容
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('.welcome').html("欢迎&nbsp;&nbsp" + name)
 
-                if (user.user_pic !== null) {
-                    // 显示图片
-                    $('.layui-nav-img').show().attr("src", user.user_pic)
-                    $('.text-avatar').hide()
-                } else {
-                    // 显示字母图片
-                    $('.layui-nav-img').hide()
-                    var text = name[0].toUpperCase();
-                    $('.text-avatar').show().html(text);
-                }
-            }
+    if (user.user_pic !== null) {
+        // 显示图片
+        $('.layui-nav-img').show().attr("src", user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        // 显示字母图片
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase();
+        $('.text-avatar').show().html(text);
+    }
+}
--- assets/js/user-avatar.js
+++ assets/js/user-avatar.js
@@ -9,3 +9,48 @@ const options = {
     }
     // 1.3 创建裁剪区域 
 $image.cropper(options)
+    // 手动触发 文件选择框的点击事件
+$('#btnChooseImage').on('click', function() {
+        $('#file').click()
+    })
+    // 为文件选择框绑定 change 事件
+$('#file').on('change', function(e) {
+
+        // 1. 拿到用户选择的文件
+        var file = e.target.files[0]
+        if (file == undefined) {
+            return layer.msg('请选择图片!')
+        }
+        // 2. 将文件，转化为路径
+        var imgURL = URL.createObjectURL(file)
+            // 3. 重新初始化裁剪区域
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', imgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    })
+    //创建一个 Canvas 画布，将 Canvas 画布上的内容，转化为 base64 格式的字符串
+$('#btnUpload').on('click', function() {
+    var dataURL = $image
+        .cropper('getCroppedCanvas', {
+            // 创建一个 Canvas 画布
+            width: 100,
+            height: 100
+        })
+        .toDataURL('image/png')
+        //调用接口，把头像上传到服务器
+    $.ajax({
+        method: 'POST',
+        url: '/my/update/avatar',
+        data: {
+            avatar: dataURL
+        },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layer.msg('更换头像失败！')
+            }
+            layer.msg('更换头像成功！')
+            window.parent.getUserInfo()
+        }
+    })
+})
